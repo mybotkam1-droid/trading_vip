4c0f9afbf624d82881a287cc10452f1fea252f52
mybotkam1-droid <mybotkam1@gmail.com>
2026-05-26 06:33:06 +0700

    Update trading_bot_lib.py

--- trading_bot_lib.py
+++ trading_bot_lib.py
@@ -805,7 +805,7 @@ def compute_signal_from_candles(prev_candle, curr_candle):
         body_prev = abs(close_prev - open_prev)
         body_curr = abs(close_curr - open_curr)
 
-        if 1.618*volume_curr > volume_prev and 1.618*body_curr > body_prev:
+        if volume_curr > 0.5*volume_prev and body_curr > 0.5*body_prev:
             if close_curr > open_curr:
                 return "BUY"
             elif close_curr < open_curr:
